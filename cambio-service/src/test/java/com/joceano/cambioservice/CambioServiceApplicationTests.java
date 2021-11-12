f0f497549b1c2334701284bc86430bfea3105b8c
joceano <alves.joceano@gmail.com>
2021-11-12 10:33:47 -0300

    created cambio service

---
+++ cambio-service/src/test/java/com/joceano/cambioservice/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joceano.cambioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
