45808651797675e1f4bab2e7acf751bb902b9cb7
joceano <alves.joceano@gmail.com>
2021-11-15 15:23:48 -0300

    load balancing with Eureka

--- book-service/src/main/java/com/joceano/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/joceano/bookservice/controller/BookController.java
@@ -32,7 +32,7 @@ public class BookController {
             throw new RuntimeException("Book not found");
         var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
         var port = environment.getProperty("local.server.port");
-        book.setEnvironment(port);
+        book.setEnvironment("Book port: " + port + " Cambio Port: " + cambio.getEnvironment());
         book.setPrice(cambio.getConvertedValue());
         return book;
     }
